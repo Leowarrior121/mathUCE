05db590bc69fcb5363ac33377f901e110d5a3441
Leowarrior121 <matiasm1116@gmail.com>
2024-06-30 18:35:51 -0500

    Add files via upload

---
+++ CODE/PROGRAMA_VOLUMEN_MATIAS_MERA.py
@@ -0,0 +1,10 @@
+
+print("Bienvenido al programa para calcular el volumen")
+print(" ")
+print("Calular el volúmen de una caja")
+L = float(input("¿Cuánto mide el lado de tu caja:"))
+P = float(input("Cuánto es la profundidad de tu caja?"))
+A = float(input("Cual es la altura de tu caja?"))
+V = L*P*A
+print ("El volumen de tu caja es de:", V, "m3")
+
